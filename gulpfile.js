cc8d82ecb7686bf7f2fe90acf6fc2b04f4d23743
Patros <Patros@Patrik>
2017-01-31 16:08:13 +0100

    Added watch and browsersync to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,29 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+
+
 
 //gulp tasks
+//gulp sass
 gulp.task('sass',function(){
     return gulp.src('src/scss/**/*.scss') //gets all files ending with scss
             .pipe(sass())
             .pipe(gulp.dest('src/css')) 
+            .pipe(broswerSync.reload({
+                stream:true
+            }))
+});
+//gulp watch
+gulp.task('watch',function(){
+    gulp.watch('src/scss/**/*.scss',['sass']);
+});
+//browserSync
+gulp.task('browserSync',function(){
+    broswerSync.init({
+        server: {
+            baseDir: 'src'
+        }
+    })
 });
